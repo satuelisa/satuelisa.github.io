050b04fe499901b12d393c32b6d34da5a97dd991
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2025-03-01 11:13:34 -0500

    clicking works now

--- teaching/hexmine/hexmine.js
+++ teaching/hexmine/hexmine.js
@@ -29,6 +29,7 @@ const UNOPENED = "#666666"; // light gray
 // DEBUG STUFF
 
 var circles = false;
+var showpos = true;
 
 
 // HEXAGONS
@@ -52,12 +53,20 @@ class Hexagon {
     }
 
     status() {
-	console.log(this.row, this.column, this.content);
+	console.log('cell', this.row, this.column, 'contains', this.counter, 'at', this.x, this.y);
+    }
+
+    activate() {
+	this.hidden = false;
+	this.draw();
+	if (this.counter == MINE) {
+	    gameover();
+	}
     }
 
     attempt() {
 	if (this.counter != MINE) {
-	    this.counter = MINE;
+	    this.couuter = MINE;
 	    // this.status();
 	    return true;
 	}
@@ -91,46 +100,54 @@ class Hexagon {
 	ctx.strokeStyle = BORDER;	
 	ctx.stroke();
 
-	/*	if (this.hidden) {
+	if (this.hidden) {
 	    ctx.fillStyle = UNOPENED;
-	} else { */
-	//}
-	
-	ctx.fillStyle = COLOR[this.counter]; // reveal content	
-	ctx.fill();
-	ctx.strokeStyle = LABEL;
-	ctx.strokeText(this.counter, this.x, this.y);
+	    ctx.fill();
+	    if (showpos) {
+		ctx.strokeStyle = LABEL;
+		let coords = this.row + ',' + this.column;
+		ctx.strokeText(coords, this.x, this.y);
+	    }
+	} else {
+	    ctx.fillStyle = COLOR[this.counter]; // reveal content	
+	    ctx.fill();
+	    ctx.strokeStyle = LABEL;
+	    ctx.strokeText(this.counter, this.x, this.y);
+	}
     }
 
     
 }
 
-function setup(w, h, s) {
-    // hexagon height
-    let hh = s * Math.sqrt(3);
+let size = 0;
+let width = 0;
+let height = 0;
+let hexheight = 0;
+let unit = 0;
 
-    ctx.font = Math.floor(s / 2) + "px Arial"; // label font
+function setup() {
+    ctx.font = Math.floor(size / 2) + "px Arial"; // label font
     
     // adjust the canvas to fit the grid
-    let ch = hh * (h + 1); // canvas height     
-    let cw = 2 * s * w;
+    let ch = hexheight * (height + 1); // canvas height     
+    let cw = 2 * size * width;
     ctx.canvas.width = cw;
     ctx.canvas.height = ch    
 
-    let x = s;
-    for (let j = 0; j < w; j++) {
-	grid.push([]);
-	let y = hh / 2; // starting one is touching the top
+    let x = size;
+    for (let j = 0; j < width; j++) {
+	grid.push([]); // first array dimension is the column
+	let y = hexheight / 2; // starting one is touching the top
 	if (j % 2 == 1) {
 	    y *= 2; // twice that on odd columns
 	}
-	for (let i = 0; i < h; i++) {
-	    var cell = new Hexagon(x, y, s, j, i);
+	for (let i = 0; i < height; i++) {
+	    var cell = new Hexagon(x, y, size, i, j);
 	    cell.draw();
-	    grid[j].push(cell);
-	    y += hh;
+	    grid[j].push(cell); // second array dimension is the row
+	    y += hexheight;
 	}
-	x += 1.5 * s;
+	x += unit;
     }
 }
 
@@ -138,16 +155,16 @@ function randint(high) {
     return Math.floor(high * Math.random());
 }
 
-function increment(c, r, w, h, span) {
+function increment(c, r, span) {
     let c1 = grid[c][r];
     let x1 = c1.x;
     let y1 = c1.y;
     for (let i = c - 1; i <= c + 1; i++) {
-	if (i < 0 || i == w) {
+	if (i < 0 || i >= width) {
 	    continue;
 	}
 	for (let j = r - 1; j <= r + 1; j++) {
-	    if (j < 0 || j == h) {
+	    if (j < 0 || j >= height) {
 		continue;
 	    }
 	    let c2 = grid[i][j];
@@ -165,14 +182,14 @@ function increment(c, r, w, h, span) {
     }
 }
     
-function place(mines, width, height, span) {
+function place(mines, span) {
     let placed = 0;
     let debugcount = 20;
     while (placed < mines) {
 	let r = randint(height);
 	let c = randint(width);
 	if (grid[c][r].attempt()) {
-	    increment(c, r, width, height, span);
+	    increment(c, r, span);
 	    placed++;
 	} 
 	debugcount--;
@@ -183,27 +200,80 @@ function place(mines, width, height, span) {
     }
 }
 
-function display(w, h, reveal) {
+function display(reveal) {
     console.log('redrawing');
-    for (let r = 0; r < h; r++) {
-	for (let c = 0; c < w; c++) {
-	    grid[c][r].draw(reveal);
+    for (let r = 0; r < height; r++) {
+	for (let c = 0; c < width; c++) {
+	    grid[c][r].draw();
 	}
     }
 
 }
 
-function create() {
-    // make sure these are numbers
-    let w = 1 * document.getElementById("width").value;
-    let h = 1 * document.getElementById("height").value;
-    let s = 1 * document.getElementById("size").value;
-    let m = 1 * document.getElementById("mines").value;    
+let active = false;
 
-    var grid = []; // reset    
-    setup(w, h, s);
-    place(m, w, h, 2.5 * s); // leave some margin for the span for floating point arithmetic
-    display(w, h, true);
+function create() {
+    // this needs to be globally set so we can deal with events efficiently    
+    // make sure these are numbers
+    width = 1 * document.getElementById("width").value;
+    height = 1 * document.getElementById("height").value;
+    size = 1 * document.getElementById("size").value;
+    unit = 1.5 * size; // a unit for rounding the event positions
+    hexheight = size * Math.sqrt(3); // hexagon height
+
+    let m = 1 * document.getElementById("mines").value;    
+    document.getElementById("create").disabled = true;    
+    
+    setup();
+    active = true;
+    place(m, 2.5 * size); // leave some margin for the span for floating point arithmetic
+    display(true);
 }
 
 
+RANGE = 3; // being lazy with the event mapping to the cells
+
+// https://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
+function click(src, e) {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    if (active) {
+	console.log('click at', x, y, '--- using unit value', unit);
+	let c = Math.floor(x / unit);
+	let r = Math.floor(y / unit);
+	console.log('approximate cell', r, c);
+	for (let i = c - RANGE; i <= c + RANGE; i++) {
+	    if (i < 0 || i >= width) {
+		continue;
+	    }
+	    for (let j = r - RANGE; j <= r + RANGE; j++) {
+		if (j < 0 || j >= height) {
+		    continue;
+		}
+		console.log(c, r);
+		let cell = grid[i][j];
+		let dx = cell.x - x;
+		let dy = cell.y - y;
+		if (Math.sqrt(dx * dx + dy * dy) < size) {
+		    cell.status();
+		    cell.activate(); // found it
+		    return;
+		}
+	    }
+	}
+    }
+}
+
+canvas.addEventListener("mousedown", click);
+
+function gameover() {
+    alert("You lost");
+    active = false;
+    for (let j = 0; j < w; j++) {
+	for (let i = 0; i < h; i++) {
+	    grid[j][i].hidden = false;
+	}
+    }
+    display(); // reveal
+}
